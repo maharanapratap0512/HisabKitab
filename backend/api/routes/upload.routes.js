c8b4262320569002f32e57d3b46ed5c675d93733
Rana Pratap <mdshivaji2512@gmail.com>
2022-04-09 13:16:04 +0530

    bug fix

--- backend/api/routes/upload.routes.js
+++ backend/api/routes/upload.routes.js
@@ -45,7 +45,23 @@ const imageMulterConfig = {
 
 const pbkImageMulterConfig = {
    storage: multer.diskStorage({
-      destination: (req, file, next) => { next(null, __dirname + "/../../../../Data/Documents/pbk"); },
+      destination: (req, file, next) => { next(null, __dirname + "/../../../../Data/Documents/Pbk"); },
+
+      filename: (req, file, next) => { next(null, file.fieldname + '_' + Date.now() + path.extname(file.originalname)); }
+   }),
+   limits: { fileSize: 5000000 }, // 1 MB = 1000000 Bytes 
+   fileFilter: (req, file, next) => {
+      if (file.mimetype != "application/pdf") {
+         file.mimetype.startsWith('image/') ? next(null, true) : next(null, false);
+      } else {
+         file.mimetype.startsWith('application/') ? next(null, true) : next(null, false);
+      }
+   }
+};
+
+const productImageMulterConfig = {
+   storage: multer.diskStorage({
+      destination: (req, file, next) => { next(null, __dirname + "/../../../../Data/Documents/Product"); },
 
       filename: (req, file, next) => { next(null, file.fieldname + '_' + Date.now() + path.extname(file.originalname)); }
    }),
@@ -101,6 +117,48 @@ router.post('/image', multer(imageMulterConfig).single('image'), async (req, res
    }
 });
 
+// Single Image for PBK
+router.post('/image/pbk', multer(pbkImageMulterConfig).single('image'), async (req, res) => {
+   try {
+      if (!req.file) {
+         res.json({
+            success: false,
+            message: 'Invalid or No file provided'
+         });
+      } else {
+         const imgPath = req.file.destination.split('.')[1] + '/' + req.file.filename;
+         res.json({
+            success: true,
+            file: req.file.path
+         });
+      }
+   } catch (err) {
+      res.status(500).json(errorHandler(err));
+   }
+});
+
+// Single Image for Product
+router.post('/image/product', multer(productImageMulterConfig).single('image'), async (req, res) => {
+   try {
+      if (!req.file) {
+         res.json({
+            success: false,
+            message: 'Invalid or No file provided'
+         });
+      } else {
+         const imgPath = req.file.destination.split('.')[1] + '/' + req.file.filename;
+         res.json({
+            success: true,
+            file: req.file.path
+         });
+      }
+   } catch (err) {
+      res.status(500).json(errorHandler(err));
+   }
+});
+
+
+
 
 //return list of all available images.
 router.get('/image', async (req, res) => {
@@ -119,12 +177,87 @@ router.get('/image', async (req, res) => {
    }
 });
 
+//return list of all pbk images.
+router.get('/image/pbk', async (req, res) => {
+   try {
+      //  console.log("WElcome");
+      fs.readdir(__dirname + "/../../../../Data/Documents/Pbk", (err, files) => {
+         res.json({
+            success: true,
+            result: files,
+            dirpath: 'public//Pbk//',
+         });
+      });
+   }
+   catch (err) {
+      res.status(500).json(errorHandler(err));
+   }
+});
 
-//return list of all available images.
+//return list of all pbk images.
+router.get('/image/product', async (req, res) => {
+   try {
+      //  console.log("WElcome");
+      fs.readdir(__dirname + "/../../../../Data/Documents/Product", (err, files) => {
+         res.json({
+            success: true,
+            result: files,
+            dirpath: 'public//Product//',
+         });
+      });
+   }
+   catch (err) {
+      res.status(500).json(errorHandler(err));
+   }
+});
+
+//delete images.
 router.delete('/image', async (req, res, next) => {
    try {
       if (req.body.filename) {
          fs.rm(__dirname + "/../../../../Data/Documents/Images/" + req.body.filename, (err) => {
+            if (err) {
+               return next(err);
+            }
+            res.json({
+               success: true,
+               result: 'file delete succesfully.'
+            })
+
+         });
+      }
+   }
+   catch (err) {
+      res.status(500).json(errorHandler(err));
+   }
+});
+
+//delete pbk image.
+router.delete('/image/pbk', async (req, res, next) => {
+   try {
+      if (req.body.filename) {
+         fs.rm(__dirname + "/../../../../Data/Documents/Pbk/" + req.body.filename, (err) => {
+            if (err) {
+               return next(err);
+            }
+            res.json({
+               success: true,
+               result: 'file delete succesfully.'
+            })
+
+         });
+      }
+   }
+   catch (err) {
+      res.status(500).json(errorHandler(err));
+   }
+});
+
+//return list of all available images.
+router.delete('/image/product', async (req, res, next) => {
+   try {
+      if (req.body.filename) {
+         fs.rm(__dirname + "/../../../../Data/Documents/Product/" + req.body.filename, (err) => {
             if (err) {
                return next(err);
             }
