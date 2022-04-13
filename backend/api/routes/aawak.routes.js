85aff61952d5603278021336777f59ac0040e922
Rana Pratap <mdshivaji2512@gmail.com>
2022-04-13 12:04:29 +0530

    awak-jwk connection

--- backend/api/routes/aawak.routes.js
+++ backend/api/routes/aawak.routes.js
@@ -36,20 +36,21 @@ router.post('/', async (req, res, next) => {
 router.post('/:dept_id', async (req, res, next) => {
     if (req.body) {
         let jawaks = [];
-        if(req.body.jawakArr){
-            jawaks = req.body.jawakArr;
-            delete req.body.jawakArr;
+        if (req.body.jawak_detail) {
+            jawaks = req.body.jawak_detail;
+            delete req.body.jawak_detail;
         }
         await DB.insertFromDept('aawak', req.body, req.params.dept_id).then(async (data) => {
             // console.log("data", data);
-                data.jawakArr = [];
-                for(let i = 0; i < jawaks.length; i++){
-                    await DB.insertFromDept('jawak', jawaks[i], req.params.dept_id).then((jwkdata) => {
-                        data.jawakArr.push(jwkdata);
-                    },(err)=>{
-                        console.log("jawak err", err, "jawak",jawaks[i]);
-                    });
-                }
+            data.jawak_detail = [];
+            for (let i = 0; i < jawaks.length; i++) {
+                jawaks[i].aawak_ref_id = data._id;
+                await DB.insertFromDept('jawak', jawaks[i], req.params.dept_id).then((jwkdata) => {
+                    data.jawak_detail.push(jwkdata);
+                }, (err) => {
+                    console.log("jawak err", err, "jawak", jawaks[i]);
+                });
+            }
             res.json({
                 success: true,
                 result: data || {}
@@ -67,6 +68,16 @@ router.post('/:dept_id', async (req, res, next) => {
 //aawak get dept
 router.get('/:dept_id', async (req, res, next) => {
     await DB.getFullListByDept('aawak', req.params.dept_id).then(async (resolve) => {
+        if (req.params.dept_id == '1') {
+            for (let i = 0; i < resolve.length; i++) {
+                let jwkconditionString = ` jawak.aawak_ref_id = ${resolve[i]._id}`;
+                await DB.getFullListByDept('jawak', req.params.dept_id, jwkconditionString).then(async (jwkdata) => {
+                    resolve[i].jawak_detail = jwkdata;
+                }, (err) => {
+                    console.log("jawak", err);
+                });
+            }
+        }
         res.json({
             success: true,
             result: resolve || [],
@@ -78,6 +89,14 @@ router.get('/:dept_id', async (req, res, next) => {
 router.get('/pending/:dept_id', async (req, res, next) => {
     let conditionString = ` where aawak.dept_id = ${req.params.dept_id} AND remaining_qty <> 0`;
     await DB.getPendingAawak(conditionString).then(async (resolve) => {
+        for (let i in resolve) {
+            let jwkconditionString = ` aawak_ref_id = ${resolve[i]._id}`;
+            await DB.getFullListByDept('jawak', req.params.dept_id, jwkconditionString).then((data) => {
+                resolve[i].jawak_detail = data;
+            }, (err) => {
+                console.log('jawak', err);
+            });
+        }
         res.json({
             success: true,
             result: resolve || [],
@@ -96,6 +115,14 @@ router.put('/pending', async (req, res, next) => {
             conditionString += (conditionString ? ` AND ` : ` where `) + `aawak.mm_id = ${req.body.mm_id}`;
         }
         await DB.getPendingAawak(conditionString).then(async (resolve) => {
+            for (let i in resolve) {
+                let jwkconditionString = ` aawak_ref_id = ${resolve[i]._id}`;
+                await DB.getFullListByDept('jawak', req.body.dept_id, jwkconditionString).then((data) => {
+                    resolve[i].jawak_detail = data;
+                }, (err) => {
+                    console.log('jawak', err);
+                });
+            }
             res.json({
                 success: true,
                 result: resolve || [],
@@ -109,27 +136,19 @@ router.put('/:dept_id', async (req, res, next) => {
     let conditionString = `1=1 ${req.body._id ? ` AND aawak._id = ${req.body._id}` : ``} ${req.body.mm_id.length > 0 ? ` AND aawak.mm_id in (${req.body.mm_id.join(',')})` : ``} ${req.body.aawak_mm_id.length > 0 ? ` AND aawak.aawak_mm_id in (${req.body.aawak_mm_id.join(',')})` : ``} ${req.body.pbk_id.length > 0 ? ` AND aawak.pbk_id in (${req.body.pbk_id.join(',')})` : ``} ${req.body.item_id.length > 0 ? ` AND aawak.item_id in (${req.body.item_id.join(',')})` : ``} ${req.body.subitem_id.length > 0 ? ` AND aawak.subitem_id in (${req.body.subitem_id.join(',')})` : ``} ${req.body.aawak_type_id.length > 0 ? ` AND aawak.aawak_type_id in (${req.body.aawak_type_id.join(',')})` : ``} ${req.body.product_id.length > 0 ? ` AND aawak.product_id in (${req.body.product_id.join(',')})` : ``} ${req.body.condition_id.length > 0 ? ` AND aawak.condition_id in (${req.body.condition_id.join(',')})` : ``} ${req.body.pkt_num ? ` AND aawak.pkt_num = ${req.body.pkt_num}` : ``} ${req.body.nimmit ? ` AND aawak.nimmit = ${req.body.nimmit}` : ``}`;
 
     await DB.getFullListByDept('aawak', req.params.dept_id, conditionString).then(async (resolve) => {
+        for (let i in resolve) {
+            let jwkconditionString = ` aawak_ref_id = ${resolve[i]._id}`;
+            await DB.getFullListByDept('jawak', req.params.dept_id, jwkconditionString).then((data) => {
+                resolve[i].jawak_detail = data;
+            }, (err) => {
+                console.log('jawak', err);
+            });
+        }
         res.json({
             success: true,
             result: resolve || [],
         });
     }, (err) => { return next(err) });
-});
-
-//  aawak get 
-router.get('/', async (req, res, next) => {
-    let bachat = [];
-    let aawak = [];
-    await DB.getList('aawak').then((res) => {
-        aawak = res || [];
-    }, (err) => { return next(err) });
-    await DB.getList('bachat').then((res) => {
-        bachat = res || [];
-    }, (err) => { return next(err) });
-    res.json({
-        success: true,
-        result: { aawakEntry: aawak || [], bachatEntry: bachat || [] }
-    });
 });
 
 //  aawak get
@@ -149,9 +168,24 @@ router.get('/', async (req, res, next) => {
 router.put('/', async (req, res, next) => {
     if (req.body.set && req.body.query) {
         let condition = 'aawak._id = ' + req.body.query._id;
+        let jawaks = [];
+        if (req.body.set.jawak_detail) {
+            jawaks = req.body.set.jawak_detail;
+            delete req.body.set.jawak_detail;
+        }
         await DB.update('aawak', req.body.set, condition, async (err, data) => {
             if (err) {
                 return next(err);
+            }
+            data.jawak_detail = [];
+            for (let i = 0; i < jawaks.length; i++) {
+                let jwkconditionString = `jawak._id = ${jawaks[i]._id}`;
+                await DB.update('jawak', jawaks[i], jwkconditionString, (err, jwkdata) => {
+                    if(err){
+                        console.log("jawak err", err, "jawak", jawaks[i]);
+                    }
+                    data.jawak_detail.push(jwkdata);
+                });
             }
             res.json({
                 success: true,
