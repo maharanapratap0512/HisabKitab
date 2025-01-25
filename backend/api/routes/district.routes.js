66553cd91694b9f8b3c2b44fab0638167ef7e081
Rana Pratap <94529075+maharanapratap0512@users.noreply.github.com>
2025-01-25 19:36:48 +0530

    district

---
+++ backend/api/routes/district.routes.js
@@ -0,0 +1,74 @@
+const router = require('express').Router();
+const DBContex = require('../models/DBContex');
+const DB = new DBContex();
+
+
+// get district by dept
+router.get('/:dept_id?', async (req, res, next) => {
+    try {
+        await DB.getList('district', { full: true }).then((resolve) => {
+            res.json({
+                success: true,
+                result: resolve.data || [],
+                total_count: (resolve.total_count ? resolve.total_count : 0),
+            });
+        });
+    } catch (err) { next(err) };
+});
+
+
+// post district 
+router.post('/', async (req, res, next) => {
+    try {
+        if (req.body && req.body.district_hin) {
+            await DB.insert('district', req.body).then(async (data) => {
+                res.json({
+                    success: true,
+                    result: data || []
+                });
+            })
+        }
+        else {
+            return next(new Error('Please fill required fields.'))
+        }
+    } catch (err) { next(err) };
+});
+
+
+// update district 
+router.put('/', async (req, res, next) => {
+    try {
+        if (req.body.set && req.body.query) {
+            await DB.update('district', req.body.set, req.body.query._id).then(async (data) => {
+                res.json({
+                    success: true,
+                    result: data || []
+                });
+            });
+        }
+        else {
+            return next(new Error('Id not found.'))
+        }
+    } catch (err) { next(err) };
+});
+
+
+// delete district 
+router.delete('/:id', async (req, res, next) => {
+    try {
+        if (req.params.id) {
+            await DB.delete('district', req.params.id).then((data) => {
+                res.json({
+                    success: true,
+                    result: data
+                });
+            })
+        }
+        else {
+            return next(new Error('Id not found.'))
+        }
+    } catch (err) { next(err) };
+});
+
+
+module.exports = router;
